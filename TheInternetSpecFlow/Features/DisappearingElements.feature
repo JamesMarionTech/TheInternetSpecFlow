22a559c6dad60c3903d9aa1bf36b4a9221068375
James Marion <jamesmariontech@pm.me>
2022-09-24 12:11:21 -0600

    Add Disappearing Elements test

---
+++ TheInternetSpecFlow/Features/DisappearingElements.feature
@@ -0,0 +1,10 @@
+﻿@DisappearingElements_Feature
+Feature: DisappearingElements
+A page with between 4 to 5 list items.
+
+Sometimes, the 5th element (Gallery) will not appear on page load.
+
+@DisappearingElements_Scenario
+Scenario: Refreshing the page enough times will achieve a different number of elements
+    Given an initial number of elements
+    Then there should be a different number of elements within 10 page refreshes
---
+++ TheInternetSpecFlow/Pages/DisappearingElementsPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace TheInternetSpecFlow.Pages
+{
+    public class DisappearingElementsPage : GeneralPage
+    {
+        public DisappearingElementsPage(BrowserDriver browser)
+            : base(browser, "/disappearing_elements")
+        { }
+
+        public int GetNumberOfElements()
+        {
+            return 
+                _browser.webDriver
+                .FindElements(By.CssSelector(".example li"))
+                .Count();
+        }
+
+        public void RefreshPage()
+        {
+            _browser.webDriver.Navigate().Refresh();
+        }
+    }
+}
